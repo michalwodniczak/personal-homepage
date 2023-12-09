7ef2e53ce7358075d8def07030c6d260f97eb6a7
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-12-09 13:22:36 +0100

    added new Colors to theme

--- src/theme.js
+++ src/theme.js
@@ -4,5 +4,8 @@ export const theme = {
     color: {
         mineShaft: "#252525",
         slateGray: "#6E7E91",
+        scienceBlue: "#0366D6",
+        white: "#FFFFFF",
+        iron: "#d1d5da;",
     }
-}
+};
