83e22b22e942669b505af936422567d48b1e89cd
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-12-11 17:01:36 +0100

    added abouMe.js

---
+++ src/common/aboutMe.js
@@ -0,0 +1,7 @@
+export const aboutMe = {
+    name: "Michał Wodniczak",
+    description: "👨🏻‍💻  I’m a passionate Frontend Developer in love with React. I am looking for my first job and programming challenges. I want to explore new technologies, improve my skills and gain knowledge among other programmer",
+    mail:"michal.wodniczak@o2.pl",
+    url:"mailto:michal.wodniczak@o2.pl",
+    footerDescription:"I’m always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come to life, feel free to conatct me."
+};
