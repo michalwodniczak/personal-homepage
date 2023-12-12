ac6876494056be528df6b86a9b6e1b537e0d05e1
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-12-12 17:24:41 +0100

    added whiteLiliac color to theme

--- src/theme.js
+++ src/theme.js
@@ -3,8 +3,10 @@ export const theme = {
         mineShaft: "#252525",
         slateGray: "#6E7E91",
         scienceBlue: "#0366D6",
+        whiteLiliac:"#FBFBFE",
         white: "#FFFFFF",
-        iron: "#d1d5da;",
+        iron: "#D1D5DA;",
         anakiwa:"#8CC2FF",
+        
     }
 };
