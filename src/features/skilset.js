5cef217117685bd3804b92ef14f06b1a755043b4
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-12-12 16:45:36 +0100

    added skilset.js

---
+++ src/features/skilset.js
@@ -0,0 +1,31 @@
+export const skills = [
+    "semantic & accesible HTML",
+    "Responsive Web Desing",
+    "Teamwork",
+    "Markdown",
+    "Immutability",
+    "CSS BEM Convention",
+    "CSS Grid",
+    "CSS Flexbox",
+    "React Router",
+    "Redux - Saga",
+    "Redux (Toolkit)",
+    "React Hooks",
+    "Error handling",
+    "Working with APi (fetch,axios)",
+    "Javascript: ES6+",
+    "Promise, Async/Await",
+    "GitHub Pull request & Review",
+    "Trello",
+    "Scrum",
+    "NPM",
+    "React",
+    "Git",
+];
+
+export const skillsToLearn = [
+    "React Context",
+    "Node.js",
+    "TypeScript",
+    "Unit testing",
+];
