1f9be2ef5b21dc005de4fc4bed413bad41f77316
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-12-15 19:17:26 +0100

    added lightBlue color to theme

--- src/theme.js
+++ src/theme.js
@@ -3,10 +3,11 @@ export const theme = {
         mineShaft: "#252525",
         slateGray: "#6E7E91",
         scienceBlue: "#0366D6",
-        whiteLiliac:"#FBFBFE",
+        whiteLiliac: "#FBFBFE",
         white: "#FFFFFF",
         iron: "#D1D5DA;",
-        anakiwa:"#8CC2FF",
-        
+        anakiwa: "#8CC2FF",
+        dustyGray: "#d1d5da",
+        lightBlue: "rgba(3, 102, 214, 0.20)"
     }
 };
