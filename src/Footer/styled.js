d2140b12a46c5d45e41cd25dfa18f06d7e7cbd24
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-12-27 15:09:31 +0100

    forrmated code and changed order in link

--- src/Footer/styled.js
+++ src/Footer/styled.js
@@ -18,23 +18,36 @@ export const Title = styled.h2`
     letter-spacing: 1.6px;
     color:${({ theme }) => theme.color.mineShaft};
 `
+
 export const Text = styled.p`
     color:${({ theme }) => theme.color.mineShaft};
 `
+
 export const Icons = styled.div`
     margin-top:56px;
     display: flex;
     gap:20px;
 `
-export const Link = styled.a`
-    color: ${({theme}) => theme.icon};
-    transition: color 0.3s;
 
+export const Link = styled.a`
+    color: ${({ theme }) => theme.color.mineShaft};
+    transition: 0.3s;
+    
+    &:visited{
+        color: ${({ theme }) => theme.color.mineShaft};
+    };
+    
+    &:focus{
+        color: ${({ theme }) => theme.color.scienceBlue};
+    };
+    
     &:hover{
-        color:${({ theme }) => theme.color.scienceBlue};
-    }
+        color: ${({ theme }) => theme.color.scienceBlue};
+    };
+
 `
 
 export const IconStyle = (Icon) => styled(Icon)`
-
+    width: 48px;
+    height: 48px;
 `
