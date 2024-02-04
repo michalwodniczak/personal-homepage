e5908293a8422654b606bb2e848cbdd2c368b191
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-02-04 22:26:24 +0100

    added media query breakpoints

--- src/features/PersonalHomepage/Portfolio/styled.js
+++ src/features/PersonalHomepage/Portfolio/styled.js
@@ -2,12 +2,23 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
     margin-top: 24px;
-    display: grid;
-    grid-template-columns: repeat(2,1fr);
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
     gap: 32px;
 
-    @media (max-width: ${({theme}) => theme.breakpoints.smallPhone}px){
+    @media (max-width: ${({theme}) => theme.breakpoints.laptop}px){
+        gap:10px;
+    };
+
+    @media (max-width: ${({theme}) => theme.breakpoints.smallLaptop}px){
+        gap:10px;
+    }
+    
+    @media (max-width: ${({theme}) => theme.breakpoints.phone}px){
         grid-template-columns: 1fr;
         gap:16px;
-    }
+    };
+
+
 `;
