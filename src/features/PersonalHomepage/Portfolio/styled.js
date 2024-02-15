c7ce59db82bab7a868dcf35c3c809f6519e9a8a3
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-02-15 16:18:52 +0100

    deleted propery from media query and changed gap

--- src/features/PersonalHomepage/Portfolio/styled.js
+++ src/features/PersonalHomepage/Portfolio/styled.js
@@ -2,18 +2,15 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
     margin-top: 24px;
-    display: flex;
-    justify-content: space-evenly;
-    flex-wrap: wrap;
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
     gap: 32px;
 
-    @media (max-width: ${({theme}) => theme.breakpoints.laptop}px){
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        gap:14px;
+    @media (max-width: ${({ theme }) => theme.breakpoints.laptop}px){
+        gap:24px;
     };
 
-    @media (max-width: ${({theme}) => theme.breakpoints.phone}px){
+    @media (max-width: ${({ theme }) => theme.breakpoints.phone}px){
         grid-template-columns: 1fr;
         gap:16px;
     };
